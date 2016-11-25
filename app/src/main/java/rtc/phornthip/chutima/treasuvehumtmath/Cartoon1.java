129c1393f2277630392f93d996cde07023cd3a5a
chompoo <beetza011727@gmail.com>
2016-11-25 09:37:14 +0700

    create 3 Acivity

---
+++ app/src/main/java/rtc/phornthip/chutima/treasuvehumtmath/Cartoon1.java
@@ -0,0 +1,13 @@
+package rtc.phornthip.chutima.treasuvehumtmath;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cartoon1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartoon1);
+    }
+}
---
+++ app/src/main/java/rtc/phornthip/chutima/treasuvehumtmath/Cartoon2.java
@@ -0,0 +1,14 @@
+package rtc.phornthip.chutima.treasuvehumtmath;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cartoon2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartoon2);
+    }
+}
